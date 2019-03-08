9283314b556406235ff0b24b4b4e074b32d3fad0
Wenda <wenda.zhao@mail.utoronto.ca>
2019-03-08 11:25:32 -0500

    Fusing TWR

--- src/modules/src/estimator_kalman.c
+++ src/modules/src/estimator_kalman.c
@@ -1055,8 +1055,9 @@ static void stateEstimatorUpdateWithDistance(distanceMeasurement_t *d)
   // Extra logging variables
   twrDist = d->distance;
   anchorID = d->anchor_ID;
-
-  stateEstimatorScalarUpdate(&H, measuredDistance-predictedDistance, d->stdDev);
+  if (S[STATE_Z] > UWB_MIN_HEIGHT){
+	  stateEstimatorScalarUpdate(&H, measuredDistance-predictedDistance, d->stdDev);
+  }
 }
 
 static void stateEstimatorUpdateWithTDOA(tdoaMeasurement_t *tdoa)
