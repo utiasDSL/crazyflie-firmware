a863481ae4b3ba4d082546c03513b752ab5f3a9c
SiQi Zhou <siqi.zhou@robotic.utias.utoronto.ca>
2019-03-26 08:15:02 -0400

    check yaw_error

--- src/modules/src/estimator_kalman.c
+++ src/modules/src/estimator_kalman.c
@@ -1048,6 +1048,15 @@ static void stateEstimatorUpdateWithPosVelYaw(posvelyawMeasurement_t *posvelyaw,
 	  float yaw_error = posvelyaw->yaw - pred_yaw;
 	  yaw_error_logback = yaw_error;
 
+	  // check yaw_error value to avoid angle discontinuity issue
+	  if (yaw_error > PI){
+		  yaw_error -= (float) 2.0 * PI;
+	  }
+
+	  if (yaw_error <= -PI){
+		  yaw_error += (float) 2.0 * PI;
+	  }
+
 	  // wrap yaw_error between (-PI, PI]
 //	  while (yaw_error > PI){
 //		  yaw_error -= (float) 2.0 * PI;
